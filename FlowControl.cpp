fb27e220b06affc61d0491950e2e1fa2a1033105
Arkhane <arkhane@okstate.edu>
2018-01-31 20:10:14 -0600

    Week03_Lab
    Week03_Lab

--- FlowControl.cpp
+++ FlowControl.cpp
@@ -3,5 +3,28 @@ using namespace std;
 
 int main() 
 {
+	char continueProgram = 'y';
 
+	do 
+	{
+		cout << "Do you want to continue (y/n)";
+		cin >> continueProgram;
+
+		switch (continueProgram)
+		{
+		case 'y':
+			cout << "Going to Continue";
+			break;
+		case 'n':
+			cout << "Going to Exit";
+			break;
+		default:
+			continueProgram = 'y';
+
+		}
+		
+	} while (continueProgram == 'y');
+	
+	return 0;
+	
 }
